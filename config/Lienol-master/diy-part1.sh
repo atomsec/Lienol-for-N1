01df418ac492d711454ff9618026a237462e48a8
atomsec <32327666+atomsec@users.noreply.github.com>
2024-07-20 16:15:38 +0900

    Update diy-part1.sh

--- config/Lienol-master/diy-part1.sh
+++ config/Lienol-master/diy-part1.sh
@@ -12,3 +12,5 @@
 # other
 # rm -rf package/utils/{ucode,fbtest}
 
+sed -i '1i src-git kenzo https://github.com/kenzok8/openwrt-packages' feeds.conf.default
+sed -i '2i src-git small https://github.com/kenzok8/small' feeds.conf.default
